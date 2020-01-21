4ba02fc65b8facbbbf69bf4b5d1226c01de9cfd4
viz <visuweshmuthukumar@gmail.com>
2020-01-21 18:46:20 +0530

    pr: add pyplot-subplot.py

---
+++ pr/pyplot-subplot.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+from math import log, log10
+
+xs  = list(range(1, 101))
+y1s = list(map(log, xs))
+y2s = list(map(log10, xs))
+
+plt.subplot(1, 2, 1)
+plt.xlabel("x")
+plt.ylabel("ln(x)")
+plt.plot(xs, y1s, color='r')
+
+plt.subplot(1, 2, 2)
+plt.xlabel("x")
+plt.ylabel("log10(x)")
+plt.plot(xs, y2s, color='b')
+
+plt.show()
