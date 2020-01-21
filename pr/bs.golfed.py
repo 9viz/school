7ebeca1e50a1ede91d3e6263bc684a7c692dbde1
viz <visuweshmuthukumar@gmail.com>
2020-01-21 20:43:00 +0530

    pr/bs-golfed: use a[m] instead of init-ing n (108 chars)

--- pr/bs.golfed.py
+++ pr/bs.golfed.py
@@ -1,3 +1,3 @@
 def f(a,k,l,h):
 	if l>h:return-1
-	m=(l+h)//2;n=a[m];return(k>n and f(a,k,l+1,h))or(k<n and f(a,k,l,h-1))or m
+	m=(l+h)//2;return(k>a[m] and f(a,k,l+1,h))or(k<a[m] and f(a,k,l,h-1))or m
