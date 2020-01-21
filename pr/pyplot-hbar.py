e82115c9fe1fc12004bd514d0522f8944c4389b2
viz <visuweshmuthukumar@gmail.com>
2020-01-21 18:53:32 +0530

    pr: fucked up the data for pie and hbar

--- pr/pyplot-hbar.py
+++ pr/pyplot-hbar.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 
 label = ["lisp" , "c" , "python" , "go" , "haskell"]
 pop   = [59     , 80  , 90       , 60   , 55       ]
-expld = [0      , 0   , 0        , 0.25 , 0        ]
 
-plt.pie(pop, explode=expld, labels=label, autopct="%.2f")
+plt.barh(label, pop)
 plt.show()
