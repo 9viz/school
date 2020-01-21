d2b10b432c1bdec529385fa949e9f5ea2b4cc95e
viz <visuweshmuthukumar@gmail.com>
2020-01-21 19:43:38 +0530

    pr: add stack interactive menu thing

---
+++ pr/stack.py
@@ -0,0 +1,25 @@
+c   = "y"
+top = -1
+s   = []
+
+print("1. push", "2. pop", "3. peek",
+      "4. display", sep="\n")
+
+while c[0].lower() == "y":
+    ch = input(">> ")
+    if ch == "1":
+        top += 1
+        s.append(int(input("enter number: ")))
+    elif ch == "2":
+        if not s: print("underflow")
+        s.pop(top)
+        top -= 1
+    elif ch == "3":
+        print(s[top], "<- top")
+    elif ch == "4":
+        for n, i in enumerate(s[::-1]):
+            print(i,
+                  "<- top\n" if n == 0 else "\n",
+                  end="")
+    else:
+        c = input("do u want to continue [y/n]? ")
