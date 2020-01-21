ccd2c1dc5943781b0d2bda4eaddad7e243439708
viz <visuweshmuthukumar@gmail.com>
2020-01-21 20:39:35 +0530

    pr/bs-golfed: use a single letter for func name (109 chars now)

--- pr/bs.golfed.py
+++ pr/bs.golfed.py
@@ -1,3 +1,3 @@
-def bs(a,k,l,h):
+def f(a,k,l,h):
 	if l>h:return-1
-	m=(l+h)//2;n=a[m];return(k>n and bs(a,k,l+1,h))or(k<n and bs(a,k,l,h-1))or m
+	m=(l+h)//2;n=a[m];return(k>n and f(a,k,l+1,h))or(k<n and f(a,k,l,h-1))or m
