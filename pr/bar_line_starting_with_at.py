3a6509667037c1ac87a4854d61eb46bbcaf6c6bc
viz <visuweshmuthukumar@gmail.com>
2020-01-21 19:23:09 +0530

    pr/bar_line_starting_with_at.py: add iterative way to solve the prob

--- pr/bar_line_starting_with_at.py
+++ pr/bar_line_starting_with_at.py
@@ -1,5 +1,17 @@
+### functional-eqsue {
 with open("in") as f:
     ls = list(filter(lambda x: x[0] != "@", f.readlines()))
 
 with open("out", "w") as f:
     f.writelines(ls)
+### }
+
+### iterative {
+# ls = []
+# with open("in") as f:
+#     for i in f.readlines():
+#         if i[0] != "@": ls.append(i)
+#
+# with open("out", "w") as f:
+#     f.writelines(ls)
+### }
